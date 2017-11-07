15b9f9630c4e0107e3a2e931d29f160993bf43e5
rahuljoban <rahuljoban@gmail.com>
2017-11-07 12:06:56 -0800

    Add files via upload

---
+++ campus-safe-master/scripts.js
@@ -0,0 +1,76 @@
+// This example requires the Visualization library. Include the libraries=visualization
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=visualization">
+
+var map, heatmap;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: {lat: 49.263068, lng: -123.244414},
+    mapTypeId: 'satellite'
+  });
+
+  heatmap = new google.maps.visualization.HeatmapLayer({
+    data: getPoints(),
+    map: map,
+    radius: 10,
+    opacity: 1.5,
+    dissipating: true, 
+  });
+}
+
+function toggleHeatmap() {
+  heatmap.setMap(heatmap.getMap() ? null : map);
+}
+
+function changeGradient() {
+  var gradient = [
+    'rgba(0, 255, 255, 0)',
+    'rgba(0, 255, 255, 1)',
+    'rgba(0, 191, 255, 1)',
+    'rgba(0, 127, 255, 1)',
+    'rgba(0, 63, 255, 1)',
+    'rgba(0, 0, 255, 1)',
+    'rgba(0, 0, 223, 1)',
+    'rgba(0, 0, 191, 1)',
+    'rgba(0, 0, 159, 1)',
+    'rgba(0, 0, 127, 1)',
+    'rgba(63, 0, 91, 1)',
+    'rgba(127, 0, 63, 1)',
+    'rgba(191, 0, 31, 1)',
+    'rgba(255, 0, 0, 1)'
+  ]
+  heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+}
+
+function changeRadius() {
+  heatmap.set('radius', heatmap.get('radius') ? null : 20);
+}
+
+function changeOpacity() {
+  heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
+}
+
+// Heatmap data: 500 Points
+function getPoints() {
+
+  //MAIN MALL
+  var locations = new Array(101);
+  var startLat = 49.259566;
+  var startLon = -123.248518;
+  var endLat = 49.268877;
+  var endLon =  -123.256107;
+  var incLat = Math.abs((startLat - endLat) / 100);
+  console.log(incLat);
+  var incLon = Math.abs((startLon - endLon) / 100);
+  console.log(incLon);
+  for (var i = 0; i < 100; i++) {
+    locations[i] = {location: new google.maps.LatLng(startLat+=incLat, startLon-=incLon), weight: .3};
+  }
+
+  //Wreck Beach
+  //var locations = new Array(100);
+  locations[101] = {location: new google.maps.LatLng(49.262518, -123.261965), weight: 3};
+  return locations;
+}
